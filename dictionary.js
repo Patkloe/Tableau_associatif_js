3a473debc96594f0f792e2c00c59bc0146a4f439
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-08-10 10:09:08 -0500

    Update dictionary.js

--- dictionary.js
+++ dictionary.js
@@ -1,3 +1,25 @@
+function test(){
+var arr = { 
+        "Mecreance": "highest", 
+        "Solange": "Innocent", 
+        "Hotel": "Plateau", 
+        "Thelligence": "sonel_mimboman",
+        "La_mere": "Un_beignet_but three_pressed",
+        "Mecreance": "over power",
+        "over power":"Mecreance"
+    }; 
+  console.log("Keys are listed below "+"\n"); 
+  
+// Loop to print keys 
+for (var key in arr) { 
+    if (arr.hasOwnProperty(key)) { 
+  
+        // Printing Keys 
+        console.log(key + "\n"); 
+    } 
+ }  
+}
+test();
 function testscript(){
  var tab = [1,2,3,4,5];
  var a = tab.length;
